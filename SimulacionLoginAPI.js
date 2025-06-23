5c7339266e53cd3a4f2c69145376a98545a45b48
josst132 <patrick.goyez@uisek.edu.ec>
2025-06-23 08:50:41 -0500

    Add files via upload

---
+++ SimulacionLoginAPI.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import LoginForm from "./LoginForm";
+
+function App() {
+    return (
+        <div className="App">
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App;
